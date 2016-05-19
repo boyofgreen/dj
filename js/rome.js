b5d5d04ad974fc777dacceed0d4b3bc223bb2fa0
Jeff Burtoft <boyofgreen@gmail.com>
2016-05-19 12:09:59 -0500

    add code for rome

--- js/rome.js
+++ js/rome.js
@@ -1,10 +1,42 @@
 
 
-if(typeof Windows !== 'undefined'){
+if(window.Windows && window.Windows.ApplicationModel){
     
+        var appServiceCatalog = Windows.ApplicationModel.AppService.AppServiceCatalog;
+
+$('#romeControl').click(function(){
+    
+    appServiceCatalog.findAppServiceProvidersAsync(appServiceName).done( function(returnObj){
+        
+        var generateDevicelist = [];
+        generateDevicelist.push(returnObj.dList)
+    } );
+    
+    GenerateList(generateDevicelist)
+})
+
+
+var appServiceConnection = new Windows.ApplicationModel.AppService.AppServiceConnection();
+     appServiceConnection.openAsync().done(function(a){
+         sendStateData(state, a)
+     } );
+    
+    
+}
+
+
+if(typeof Windows !== 'undefined'){
+ 
+ 
+ 
+ 
     
 
     
     setTimeout(function(){ $('#romeControl').addClass('fadeIn')}, 4000)
     
 }
+
+
+var GenerateList = function(a){             };
+var sentStateData = function(state, a){         };
